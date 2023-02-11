8f0f129d2ae2c237629cc3eeac65a9df4a399220
BataryByte <osintmanmain@gmail.com>
2023-02-11 14:46:20 +0100

    incr5

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,7 +7,7 @@ import (
 
 type Config struct {
 	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"8080"`
-	BaseURL       string `env:"BASE_URL" envDefault:"shorten"`
+	BaseURL       string `env:"BASE_URL" envDefault:"api"`
 }
 
 //var instance *Config

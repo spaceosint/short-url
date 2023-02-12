fc868368f5c87d3e0d70413bfcfcb3e6b54301b2
BataryByte <osintmanmain@gmail.com>
2023-02-12 20:59:10 +0100

    incr6

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,9 +6,9 @@ import (
 )
 
 type Config struct {
-	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"127.0.0.1:8080"`
-	BaseURL         string `env:"BASE_URL" envDefault:"http://127.0.0.1:8080"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"file"`
+	ServerAddress   string `env:"SERVER_ADDRESS" `    // envDefault:"127.0.0.1:8080"
+	BaseURL         string `env:"BASE_URL"`           // envDefault:"http://127.0.0.1:8080"
+	FileStoragePath string `env:"FILE_STORAGE_PATH" ` // envDefault:"file"
 }
 
 //var instance *Config

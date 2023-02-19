f1abb4fb8f62f8010771c0ca55f2be2f9590fc2e
batarybyte <osintmanmain@gmail.com>
2023-02-19 21:26:36 +0100

    incr9

--- internal/middleware/middelware.go
+++ internal/middleware/middelware.go
@@ -27,7 +27,7 @@ func GzipHandle() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 		// проверяем, что клиент поддерживает gzip-сжатие
-		if !strings.Contains(c.Request.Header.Get("Content-Encoding"), "gzip") {
+		if !strings.Contains(c.Request.Header.Get("Content-Type"), "gzip") {
 			// если gzip не поддерживается, передаём управление
 			// дальше без изменений
 			c.Next()
